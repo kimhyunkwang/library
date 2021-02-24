2ac741c6616b6a414f4cb63c7b778c5c02d2d1fc
kimhyunkwang <khkhy8712@gmail.com>
2021-02-24 10:36:01 +0900

    update: 로그인 오류 수정

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 import pymysql
-from flask import Flask, jsonify, request, session
+from flask import Flask, request, session, render_template, url_for, redirect
 from flask_restful import reqparse, abort, Api, Resource
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
@@ -17,62 +17,89 @@ db = pymysql.connect(
 
 cursor = db.cursor()
 
-# session을 위한 secret_key 설정
 app.config.from_mapping(SECRET_KEY='dev')
-
-parser = reqparse.RequestParser()
-parser.add_argument('id')
-parser.add_argument('email')
-parser.add_argument('fullname')
-parser.add_argument('password')
-
 
 @app.route("/")
 def home():
-    if session['user_id']:
+    if session.get('logged_in'):
         return render_template('loggedin.html')
     else:
         return render_template('index.html') 
 
 
-@app.route('/user/register', methods=('GET', 'POST'))
+@app.route('/register', methods=('GET', 'POST'))
 def register():
-    args = parser.parse_args()
     if request.method == 'POST':
-        sql = "INSERT INTO `user` (`fullname`, `email`, `password`) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (args['fullname'], args['email'], generate_password_hash(args['password'])))
-        db.commit()
-        return redirect(url_for('login'))
-    else:
-        return render_template('register.html')
+        fullname = request.form['fullname']
+        email = request.form['email']
+        password = request.form['password']
+        repeat_password = request.form['repeat-password']
+        
+        error = None
+        
+        if not fullname:
+            error = 'Fullname이 유효하지 않습니다.'
+        if not email:
+            error = 'Email이 유효하지 않습니다.'
+        elif not password:
+            error = 'Password가 유효하지 않습니다.'
+        elif not repeat_password:
+            error = 'Repeat Password가 유효하지 않습니다.'
 
+        sql = 'SELECT id FROM user WHERE email = %s'
+        cursor.execute(sql, (email,))
+        result = cursor.fetchone()
+        if result is not None:
+            error = '{} 은 이미 등록된 계정입니다.'.format(email)
 
-@app.route('/user/login', methods=('GET', 'POST'))
-def login():
-    args = parser.parse_args()
-    if request.method == 'POST':
-        sql = "SELECT password FROM `user` WHERE `email` = %s"
-        cursor.execute(sql, (args["email"], ))
-        user_info = cursor.fetchone()
-        if user_info:
-            if check_password_hash(user_info[0], args["password"]):
-                session.clear()
-                session['user_id'] = args["email"]
-	
-                return redirect(url_for('home'))
-            else:
-                return "비밀번호를 확인해주세요."
+        if password != repeat_password:
+            error = 'Password와 Repeat Password가 다릅니다.'
+
+        if error is None:
+            sql = "INSERT INTO `user` (`email`, `fullname`, `password`) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (email, fullname, generate_password_hash(password)))
+            db.commit()
+            return redirect(url_for('login'))
         else:
-            return "존재하지 않는 계정입니다."
-    else:
-        return render_template('login.html')
+            return render_template('register.html', error=error)
+        
+    return render_template('register.html')
 
 
-@app.route('/user/logout')
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        error = None
+        
+        sql = 'SELECT email, password FROM user WHERE email = %s'
+        cursor.execute(sql, (email,))
+        user = cursor.fetchone()
+        
+        if user is None:
+            error = '등록되지 않은 계정입니다.'
+        
+        if not (user == None or check_password_hash(user[1], password)):
+            error = '비밀번호가 틀렸습니다.'
+
+        if error is None:
+            session.clear()
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
 def logout():
-    session.clear()
+    session['logged_in'] = False
     return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run('localhost', port=5000)
