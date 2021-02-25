99ae967bd2eaf318eebfc3acac1fd13b8adba166
kimhyunkwang <khkhy8712@gmail.com>
2021-02-26 00:33:05 +0900

    update: Book SQLAlchemy 모델 생성

--- launcher/models.py
+++ launcher/models.py
@@ -13,15 +13,29 @@ class User(db.Model):
         self.email = email
         self.password = password
 
-# class Book(db.model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     bookname = db.Column(db.String(64), nullable=False)
-#     publisher = db.Column(db.String(64), nullable=False) 
-#     author = db.Column(db.String(64), nullable=False) 
-#     publication_date = db.Column(db.Date(), nullable=False)
-#     pages = db.Column(db.Integer, nullable=False)
-#     isbn = db.Column(db.String(64), nullable=False)
-#     description = db.Column(db.Text, nullable=False)
-#     link = db.Column(db.String(128), nullable=False)
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(64), nullable=False)
+    publisher = db.Column(db.String(64), nullable=False) 
+    author = db.Column(db.String(64), nullable=False) 
+    publication_date = db.Column(db.Date(), nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    link = db.Column(db.Text, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image_path = db.Column(db.String(128), nullable=False)
 
-#     def __init__(self, )
+    def __init__(self, book_name, publisher, author, publication_date, pages, isbn, description, link, stock, rating, image_path):
+        self.book_name = book_name
+        self.publisher = publisher
+        self.author = author
+        self.publication_date = publication_date
+        self.pages = pages
+        self.isbn = isbn
+        self.description = description
+        self.link = link
+        self.stock = stock
+        self.rating = rating
+        self.image_path = image_path
