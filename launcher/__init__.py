6a53f6d818f83b5133b06899c9e050ff353858d4
kimhyunkwang <khkhy8712@gmail.com>
2021-02-28 11:09:54 +0900

    update: 대여하기 기능 조건문 수정

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -103,16 +103,15 @@ def create_app():
 
             # 재고가 있는 경우
             if book.stock > 0:
-
-                book_rental = BookRental.query.filter(BookRental.book_id == book_id, BookRental.user_id == user_id).first()
-                # 대여해서 반납하지 않은 경우
+                book_rental = BookRental.query.filter(BookRental.book_id == book_id, BookRental.user_id == user_id, BookRental.return_date == None).first()
+                
+                # 대여해서 반납하지 않은 경우 -> 대여 불가
                 if book_rental:
-                    if book_rental.return_date:
-                        flash("고객님께서 대여 중인 책입니다.")
-                        books = Book.query.all()
-                            return render_template('main.html', books=books)
+                    flash("고객님께서 대여 중인 책입니다.")
+                    books = Book.query.all()
+                    return render_template('main.html', books=books)
 
-                # 대여한 적이 없는 경우와 대여한 적 있지만 반납 완료한 경우
+                # 대여한 적이 없는 경우와 대여한 적 있지만 반납 완료한 경우 -> 대여 가능
                 now = datetime.datetime.now()
                 nowDate = now.strftime('%Y-%m-%d')
 
