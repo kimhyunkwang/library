abce7e8fef1565f59de389e3fe631f0a3305cf8b
kimhyunkwang <khkhy8712@gmail.com>
2021-03-09 10:57:52 +0900

    delete: config.py

--- launcher/config.py
+++
@@ -1,4 +0,0 @@
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://root:@localhost:3306/library?charset=utf8")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'dev'
